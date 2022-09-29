0ebad2a0353c72f0e9e54f278158fe2bac2d79e1
fazlarabby1 <fazlarabby41@gmail.com>
2022-09-29 14:47:32 +0600

    added button in the cards

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -13,6 +13,7 @@ const Exercise = (props) => {
                     <p class="card-text">{description.length < 140 ? description : description.slice(0 , 140)}</p>
                     <p><strong>Age:</strong> {age}</p>
                     <p><strong>Time:</strong> {time}m</p>
+                    <button className='bg-primary text-white fw-bold border-0 rounded w-100 py-2'>Add to list</button>
                 </div>
                 </div>
             </div>
