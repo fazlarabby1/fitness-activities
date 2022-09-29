9c6388fe1c9b6edda1ce416becbc430f9257ab4f
fazlarabby1 <fazlarabby41@gmail.com>
2022-09-29 15:42:32 +0600

    added page title and icons by fontawesome

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react';
 import Detail from '../Detail/Detail';
 import Exercise from '../Exercise/Exercise';
@@ -13,12 +15,15 @@ const Activity = () => {
          [])
     return (
         <div className="activity">
-            <div className="exercise-container mt-5 row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-3 g-4">
+            <div className='exercise-container'>
+                <h1> <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Rush Fitness Activities</h1>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-3 g-4">
                 {
                    exercises.map(exercise => <Exercise
                    key={exercise.id} exercise={exercise}>
                    </Exercise>)
                 }
+            </div>
             </div>
             <div className="detail-container">
                 <Detail></Detail>
